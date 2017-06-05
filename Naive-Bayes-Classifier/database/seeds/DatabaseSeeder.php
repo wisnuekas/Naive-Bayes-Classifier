951899c340fb30f7072a176ea0f8871b6c57dfb2
arnaz adiputra <arnazadiputra@gmail.com>
2017-06-05 22:36:37 +0700

    buat migrasi dan data training

--- Naive-Bayes-Classifier/database/seeds/DatabaseSeeder.php
+++ Naive-Bayes-Classifier/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,223 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+//         $this->call(seed_datatrining::class);
+        DB::table('datatrinings')->insert([
+            'age' => '1',
+            'spectacle' => '1',
+            'asigmatic' => '1',
+            'tear' => '1',
+            'output' => '3',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '1',
+            'spectacle' => '1',
+            'asigmatic' => '1',
+            'tear' => '2',
+            'output' => '2',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '1',
+            'spectacle' => '1',
+            'asigmatic' => '2',
+            'tear' => '1',
+            'output' => '3',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '1',
+            'spectacle' => '1',
+            'asigmatic' => '2',
+            'tear' => '2',
+            'output' => '1',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '1',
+            'spectacle' => '2',
+            'asigmatic' => '1',
+            'tear' => '1',
+            'output' => '3',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '1',
+            'spectacle' => '2',
+            'asigmatic' => '1',
+            'tear' => '2',
+            'output' => '2',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '1',
+            'spectacle' => '2',
+            'asigmatic' => '2',
+            'tear' => '1',
+            'output' => '3',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '1',
+            'spectacle' => '2',
+            'asigmatic' => '2',
+            'tear' => '2',
+            'output' => '1',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '2',
+            'spectacle' => '1',
+            'asigmatic' => '1',
+            'tear' => '1',
+            'output' => '3',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '2',
+            'spectacle' => '1',
+            'asigmatic' => '1',
+            'tear' => '2',
+            'output' => '2',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '2',
+            'spectacle' => '1',
+            'asigmatic' => '2',
+            'tear' => '1',
+            'output' => '3',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '2',
+            'spectacle' => '1',
+            'asigmatic' => '2',
+            'tear' => '2',
+            'output' => '1',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '2',
+            'spectacle' => '2',
+            'asigmatic' => '1',
+            'tear' => '1',
+            'output' => '3',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '2',
+            'spectacle' => '2',
+            'asigmatic' => '1',
+            'tear' => '2',
+            'output' => '2',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '2',
+            'spectacle' => '2',
+            'asigmatic' => '2',
+            'tear' => '1',
+            'output' => '3',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '2',
+            'spectacle' => '2',
+            'asigmatic' => '2',
+            'tear' => '2',
+            'output' => '3',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '3',
+            'spectacle' => '1',
+            'asigmatic' => '1',
+            'tear' => '1',
+            'output' => '3',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '3',
+            'spectacle' => '1',
+            'asigmatic' => '1',
+            'tear' => '2',
+            'output' => '3',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '3',
+            'spectacle' => '1',
+            'asigmatic' => '2',
+            'tear' => '1',
+            'output' => '3',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '3',
+            'spectacle' => '1',
+            'asigmatic' => '2',
+            'tear' => '2',
+            'output' => '1',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '3',
+            'spectacle' => '2',
+            'asigmatic' => '1',
+            'tear' => '1',
+            'output' => '3',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '3',
+            'spectacle' => '2',
+            'asigmatic' => '1',
+            'tear' => '2',
+            'output' => '2',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '3',
+            'spectacle' => '2',
+            'asigmatic' => '2',
+            'tear' => '1',
+            'output' => '3',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+        DB::table('datatrinings')->insert([
+            'age' => '3',
+            'spectacle' => '2',
+            'asigmatic' => '2',
+            'tear' => '2',
+            'output' => '3',
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now(),
+        ]);
+
     }
 }
